4eb0d8f3dbcee1d39f8b54d709adb7f508d5d1fb
immortalityidle <103467158+immortalityidle@users.noreply.github.com>
2022-04-15 19:54:21 -0600

    Added health and stuff

--- src/app/game-state/character.ts
+++ src/app/game-state/character.ts
@@ -14,4 +14,18 @@ export class Character {
     intelligence: 0,
     charisma: 0
   }
+  status = {
+    health: {
+      current: 100,
+      max: 100
+    },
+    stamina: {
+      current: 100,
+      max: 100
+    },
+    mana: {
+      current: 100,
+      max: 100
+    }
+  }
 }
--- src/app/health-panel/health-panel.component.ts
+++ src/app/health-panel/health-panel.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { GameStateService } from '../game-state/game-state.service';
 
 @Component({
   selector: 'app-health-panel',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HealthPanelComponent implements OnInit {
 
-  constructor() { }
+  constructor(public gameStateService: GameStateService) { }
 
   ngOnInit(): void {
   }
