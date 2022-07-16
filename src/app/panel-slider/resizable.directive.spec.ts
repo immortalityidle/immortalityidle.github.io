8718f3f027334c78678e0e5c4023f58093823503
James Bradley <jimboradley@gmail.com>
2022-07-16 11:45:51 -0600

    Remove test file because it wasn't actually testing anything

--- src/app/panel-slider/resizable.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { ResizableDirective } from './resizable.directive';
-
-describe('ResizableDirective', () => {
-  it('should create an instance', () => {
-    const directive = new ResizableDirective();
-    expect(directive).toBeTruthy();
-  });
-});
