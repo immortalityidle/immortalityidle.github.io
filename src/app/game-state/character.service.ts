987d1ecfbc4298e7e34e74bdcdbf99cd59768234
cqkok <admin@cqkok.com>
2022-07-22 18:06:55 +0800

    Only change the spirituality value of bloodline 5

--- src/app/game-state/character.service.ts
+++ src/app/game-state/character.service.ts
@@ -232,13 +232,11 @@ export class CharacterService {
     this.logService.addLogMessage(
       "You will be reborn into your own family line and reap greater benefits from your previous lives.",
       'STANDARD', 'STORY');
-    // Bloodline 1 Cost: base cost
-    // Bloodline 2 Cost: 1,000 x base cost
-    // Bloodline 3 Cost: 750,000 x base cost
-    // Bloodline 4 Cost: 375,000,000 x base cost
-    // Bloodline 5 Cost: 93,750,000,000 x base cost
-    this.characterState.bloodlineCost *= 1000 - 250 * this.characterState.bloodlineRank;
+    this.characterState.bloodlineCost *= 1000;
     this.characterState.bloodlineRank++;
+    if (this.characterState.bloodlineRank >= 4) {
+      this.characterState.bloodlineCost /= 10;
+    }
     this.resetAptitudes();
   }
 
