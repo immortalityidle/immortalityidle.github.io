f0ca41f51bb91942fda6af47076ab49a9a407cc8
Hornsoup <109191012+Hornsoup@users.noreply.github.com>
2022-08-17 22:11:38 -0700

    Bugfix: Inactive gains + offline modal
    - Added very simple modal which can be expanded with more loading information
    - fixed over-rewarding offline gains for inactive window
    - some formatting.

---
+++ src/app/offline-modal/offline-modal.component.ts
@@ -0,0 +1,16 @@
+import { Component, Inject, OnInit } from '@angular/core';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+
+@Component({
+  selector: 'app-offline-modal',
+  templateUrl: './offline-modal.component.html',
+  styleUrls: ['./offline-modal.component.less']
+})
+export class OfflineModalComponent implements OnInit {
+
+  constructor(@Inject(MAT_DIALOG_DATA) public data: { earnedTicks: number }) { }
+
+  ngOnInit(): void {
+  }
+
+}
