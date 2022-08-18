2e3e82d462fa5f509d1cbc2d763d6840a32291c4
immortalityidle <user@DESKTOP-4AK7258.localdomain>
2022-08-17 18:10:00 -0600

    Changed all tooltips to matTooltips.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,9 +17,10 @@ import { MatDialogModule } from '@angular/material/dialog';
 import { MatIconModule } from '@angular/material/icon';
 import { MatTabsModule } from '@angular/material/tabs';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MAT_TOOLTIP_DEFAULT_OPTIONS } from '@angular/material/tooltip';
 import { CommonModule, TitleCasePipe } from '@angular/common';
 import { ManualStoreModalComponent } from './manual-store-modal/manual-store-modal.component';
-import { TooltipModule } from 'ng2-tooltip-directive';
 import { BattlePanelComponent } from './battle-panel/battle-panel.component';
 import { FurnitureStoreModalComponent } from './furniture-store-modal/furniture-store-modal.component';
 import { AscensionStoreModalComponent } from './ascension-store-modal/ascension-store-modal.component';
@@ -44,7 +45,8 @@ import { SaveModalComponent } from './save-modal/save-modal.component';
 const materialModules = [
   MatDialogModule,
   MatIconModule,
-  MatTabsModule
+  MatTabsModule,
+  MatTooltipModule
 ];
 
 @NgModule({
@@ -89,7 +91,6 @@ const materialModules = [
     BrowserAnimationsModule,
     FormsModule,
     CommonModule,
-    TooltipModule.forRoot({'show-delay': 500, 'hideDelay': 50, 'hideDelayAfterClick': 50}),
     ...materialModules
   ],
   exports: [
@@ -98,7 +99,8 @@ const materialModules = [
   providers: [
     TitleCasePipe,
     BigNumberPipe,
-    MatSnackBar
+    MatSnackBar,
+    { provide: MAT_TOOLTIP_DEFAULT_OPTIONS, useValue: { disableTooltipInteractivity:true } }
   ],
   bootstrap: [AppComponent]
 })
