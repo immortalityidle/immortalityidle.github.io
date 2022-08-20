3af102fc539aecc18070fc544fe2e7219d0ac677
Hornsoup <109191012+Hornsoup@users.noreply.github.com>
2022-08-19 17:20:18 -0700

    Trivial: Formatting

--- src/app/game-state/game-state.service.ts
+++ src/app/game-state/game-state.service.ts
@@ -77,8 +77,8 @@ export class GameStateService {
     });
   }
 
-  changeAutoSaveInterval(interval: number): void{
-    if(!interval || interval < 1) {
+  changeAutoSaveInterval(interval: number): void {
+    if (!interval || interval < 1) {
       interval = 1;
     } else if (interval > 900) {
       interval = 900;
@@ -106,7 +106,7 @@ export class GameStateService {
 
   savetoLocalStorage(): void {
     const saveCopy = window.localStorage.getItem(LOCAL_STORAGE_GAME_STATE_KEY + this.getDeploymentFlavor() + this.saveSlot)
-    if (saveCopy){
+    if (saveCopy) {
       window.localStorage.setItem("BACKUP" + LOCAL_STORAGE_GAME_STATE_KEY + this.getDeploymentFlavor() + this.saveSlot, saveCopy);
     }
     window.localStorage.setItem(LOCAL_STORAGE_GAME_STATE_KEY + this.getDeploymentFlavor() + this.saveSlot, this.getGameExport());
@@ -121,16 +121,16 @@ export class GameStateService {
       return false;
     }
     this.importGame(gameStateSerialized);
-    if(this.isImport){
+    if (this.isImport) {
       this.characterService.toast("Load Successful")
       this.updateImportFlagKey(false);
     }
     return true;
   }
 
-  importGame(value: string){
+  importGame(value: string) {
     let gameStateSerialized: string;
-    if (value.substring(0, 3) === "iig"){
+    if (value.substring(0, 3) === "iig") {
       // it's a new save file
       gameStateSerialized = decodeURIComponent(atob(value.substring(3)));
     } else {
@@ -145,8 +145,8 @@ export class GameStateService {
     this.homeService.setProperties(gameState.home);
     this.inventoryService.setProperties(gameState.inventory);
     // restore functions to itemStacks, because JSON stringification throws them away
-    for (const itemStack of this.inventoryService.itemStacks){
-      if (itemStack === null){
+    for (const itemStack of this.inventoryService.itemStacks) {
+      if (itemStack === null) {
         continue;
       }
       const item = this.itemRepoService.getItemById(itemStack.item.id);
@@ -169,7 +169,7 @@ export class GameStateService {
     this.updateImportFlagKey();
   }
 
-  getGameExport(): string{
+  getGameExport(): string {
     const gameState: GameState = {
       achievements: this.achievementService.getProperties(),
       impossibleTasks: this.impossibleTaskService.getProperties(),
@@ -206,23 +206,23 @@ export class GameStateService {
   }
 
   cheat(): void {
-    this.logService.addLogMessage("You dirty cheater! You pressed the cheat button!","STANDARD","EVENT");
+    this.logService.addLogMessage("You dirty cheater! You pressed the cheat button!", "STANDARD", "EVENT");
     this.characterService.characterState.money += 1e10;
-    for (const key in this.itemRepoService.items){
+    for (const key in this.itemRepoService.items) {
       const item = this.itemRepoService.items[key];
       if (item.type === 'manual' && item.use) {
         item.use();
       }
     }
     const keys = Object.keys(this.characterService.characterState.attributes) as AttributeType[];
-    for (const key in keys){
+    for (const key in keys) {
       const attribute = this.characterService.characterState.attributes[keys[key]];
       attribute.aptitude += 1e7;
       attribute.value += 1e7;
     }
     this.inventoryService.addItem(this.inventoryService.generateSpiritGem(25));
     this.homeService.upgradeToNextHome();
-    while (this.homeService.upgrading){
+    while (this.homeService.upgrading) {
       this.homeService.upgradeTick();
     }
   }
@@ -233,16 +233,15 @@ export class GameStateService {
 
   getSaveFile() {
     const saveString = window.localStorage.getItem("saveSlotFor" + LOCAL_STORAGE_GAME_STATE_KEY + this.getDeploymentFlavor())
-    if(!saveString)
-    {
+    if (!saveString) {
       return;
     }
     this.saveSlot = saveString;
   }
 
-  getDeploymentFlavor(){
+  getDeploymentFlavor() {
     let href = window.location.href;
-    if (href === "http://localhost:4200/"){
+    if (href === "http://localhost:4200/") {
       // development, use the standard save
       return "";
     } else if (href === "https://immortalityidle.github.io/" || href === "https://immortalityidle.github.io/old/") {
--- src/app/game-state/store.service.ts
+++ src/app/game-state/store.service.ts
@@ -38,13 +38,13 @@ export class StoreService {
 
   }
 
-  setStoreInventory(){
+  setStoreInventory() {
     this.furniture = [];
-    for (const key in this.itemRepoService.furniture){
+    for (const key in this.itemRepoService.furniture) {
       const furniture = this.itemRepoService.furniture[key];
-      if (this.homeService.home.furnitureSlots.includes(furniture.slot)){
+      if (this.homeService.home.furnitureSlots.includes(furniture.slot)) {
         this.furniture.push(furniture);
-        if (this.homeService.ownedFurniture.includes(furniture.name)){
+        if (this.homeService.ownedFurniture.includes(furniture.name)) {
           this.furniturePrices[furniture.name] = 0;
         } else {
           this.furniturePrices[furniture.name] = furniture.value;
@@ -55,17 +55,17 @@ export class StoreService {
 
   }
 
-  unlockManual(manual: Item){
-    if (!this.manuals.includes(manual)){
+  unlockManual(manual: Item) {
+    if (!this.manuals.includes(manual)) {
       this.manuals.push(manual);
     }
   }
 
-  buyManual(){
-    if (this.selectedItem){
-      if (this.selectedItem.value < this.characterService.characterState.money){
+  buyManual() {
+    if (this.selectedItem) {
+      if (this.selectedItem.value < this.characterService.characterState.money) {
         this.characterService.characterState.money -= this.selectedItem.value;
-        if (this.selectedItem.type === 'manual' && this.selectedItem.use){
+        if (this.selectedItem.type === 'manual' && this.selectedItem.use) {
           // use manuals immediately
           this.selectedItem.use();
         } else {
@@ -90,16 +90,16 @@ export class StoreService {
     return this.furniture.filter(item => item.slot === slot);
   }
 
-  buyFurniture(item: Item | null = null){
+  buyFurniture(item: Item | null = null) {
     if (item === null) item = this.selectedItem;
 
-    if (item){
+    if (item) {
       if (!instanceOfFurniture(item)) {
         return;
       }
       const slot = item.slot;
-      if (item.value < this.characterService.characterState.money || this.homeService.ownedFurniture.includes(item.name)){
-        if (!this.homeService.ownedFurniture.includes(item.name)){
+      if (item.value < this.characterService.characterState.money || this.homeService.ownedFurniture.includes(item.name)) {
+        if (!this.homeService.ownedFurniture.includes(item.name)) {
           // only pay for it once per lifetime
           this.characterService.characterState.money -= item.value;
           this.homeService.ownedFurniture.push(item.name);
@@ -111,84 +111,84 @@ export class StoreService {
     }
   }
 
-  updateAscensions(){
+  updateAscensions() {
     this.soulCoreRank = this.characterService.soulCoreRank();
     this.meridianRank = this.characterService.meridianRank();
-    if (this.characterService.characterState.bloodlineRank === 0){
+    if (this.characterService.characterState.bloodlineRank === 0) {
       this.bloodlineLabel = "Establish Bloodline";
     } else {
       this.bloodlineLabel = "Enhance Bloodline";
     }
-    if (this.characterService.characterState.bloodlineRank === 0){
+    if (this.characterService.characterState.bloodlineRank === 0) {
       // Weapons
       this.bloodlineDescription = "End your current life, sacrifice all attributes and aptitudes that are not protected by the power of your previous soul core ascensions, and establish a bloodline. All of your future reincarnations will be born as your own descendants. Your weapons equipped on death will become family heirlooms and will be inherited by your future self.";
       this.bloodLineHomeRequirement = this.homeService.homesList[HomeType.Mansion];
-    } else if (this.characterService.characterState.bloodlineRank === 1){
+    } else if (this.characterService.characterState.bloodlineRank === 1) {
       // Armor
       this.bloodlineDescription = "End your current life, sacrifice all attributes and aptitudes that are not protected by the power of your previous soul core ascensions, and enhance your bloodline. Your armor and your weapons equipped on death will become family heirlooms and will be inherited by your future self.";
       this.bloodLineHomeRequirement = this.homeService.homesList[HomeType.Palace];
-    } else if (this.characterService.characterState.bloodlineRank === 2){
+    } else if (this.characterService.characterState.bloodlineRank === 2) {
       // Inherit Money
       this.bloodlineDescription = "End your current life, sacrifice all attributes and aptitudes that are not protected by the power of your previous soul core ascensions, and enhance your bloodline. Your armor and your weapons equipped on death will become family heirlooms and will be inherited by your future self. You will also inherit some of your past self's money.";
       this.bloodLineHomeRequirement = this.homeService.homesList[HomeType.Castle];
-    } else if (this.characterService.characterState.bloodlineRank === 3){
+    } else if (this.characterService.characterState.bloodlineRank === 3) {
       // Interest
       this.bloodlineDescription = "End your current life, sacrifice all attributes and aptitudes that are not protected by the power of your previous soul core ascensions, and enhance your bloodline. Your armor and your weapons equipped on death will become family heirlooms and will be inherited by your future self. You will also inherit your past self's money plus interest.";
       this.bloodLineHomeRequirement = this.homeService.homesList[HomeType.Fortress];
-    } else if (this.characterService.characterState.bloodlineRank === 4){
+    } else if (this.characterService.characterState.bloodlineRank === 4) {
       // Basic Stat Lifespan
       this.bloodlineDescription = "End your current life, sacrifice all attributes and aptitudes that are not protected by the power of your previous soul core ascensions, and enhance your bloodline. Your armor and your weapons equipped on death will become family heirlooms and will be inherited by your future self. You will also inherit your past self's money plus interest. Your aptitudes extend your lifespan to a much greater degree.";
       this.bloodLineHomeRequirement = this.homeService.homesList[HomeType.Mountain];
-    } else if (this.characterService.characterState.bloodlineRank === 5){
+    } else if (this.characterService.characterState.bloodlineRank === 5) {
       // Home
       this.bloodlineDescription = "End your current life, sacrifice all attributes and aptitudes that are not protected by the power of your previous soul core ascensions, and enhance your bloodline. You will keep your ancestral Home. You will keep your weapons, armor, and money with interest. ";
       this.bloodLineHomeRequirement = this.homeService.homesList[HomeType.ForbiddenCity];
-    } else if (this.characterService.characterState.bloodlineRank === 6){
+    } else if (this.characterService.characterState.bloodlineRank === 6) {
       // Entourage
       this.bloodlineDescription = "End your current life, sacrifice all attributes and aptitudes that are not protected by the power of your previous soul core ascensions, and enhance your bloodline. Your followers will have enhanced bloodlines and will follow you between incarnations. You will keep your weapons, armor, money with interest, your home and your Empire.";
       this.bloodLineHomeRequirement = this.homeService.homesList[HomeType.Capital];
-    } else if (this.characterService.characterState.bloodlineRank === 7){
+    } else if (this.characterService.characterState.bloodlineRank === 7) {
       // Limit Break
       this.bloodlineDescription = "End your current life, sacrifice all attributes and aptitudes that are not protected by the power of your previous soul core ascensions, and enhance your bloodline. Break through the limits of humanity. You will keep your weapons, armor, money with interest, your home, and your Empire.";
       this.bloodLineHomeRequirement = this.homeService.homesList[HomeType.ImperialSeat];
-    } else if (this.characterService.characterState.bloodlineRank === 8){
+    } else if (this.characterService.characterState.bloodlineRank === 8) {
       // Daily Aptitude Increase
       this.bloodlineDescription = "End your current life, sacrifice all attributes and aptitudes that are not protected by the power of your previous soul core ascensions, and enhance your bloodline one last time. Finally, you will gain aptitudes every day from your current Attributes, improving constantly. You will keep your weapons, armor, money with interest, your home, and your Empire.";
       this.bloodLineHomeRequirement = this.homeService.homesList[HomeType.Godthrone];
-    } else if (this.characterService.characterState.bloodlineRank === 9){
+    } else if (this.characterService.characterState.bloodlineRank === 9) {
       this.bloodlineDescription = "You can't enhance your bloodline any further. Your armor and your weapons equipped on death will become family heirlooms and will be inherited by your future self. You will also inherit your past self's money plus interest. Your aptitudes extend your lifespan to a much greater degree. Your followers also have enhanced bloodlines and will follow you between incarnations. You will keep your Home and your Empire, and can break through the limits of humanity. Finally, you will gain aptitudes every day from your current Attributes.";
     }
 
   }
 
-  condenseSoulCore(){
-    if (this.soulCoreRank >= 9){
-      this.logService.addLogMessage("You can't condense your soul core any further.","INJURY","EVENT");
+  condenseSoulCore() {
+    if (this.soulCoreRank >= 9) {
+      this.logService.addLogMessage("You can't condense your soul core any further.", "INJURY", "EVENT");
       return;
     }
-    if (this.characterService.characterState.attributes.spirituality.value < this.characterService.characterState.condenseSoulCoreCost){
-      this.logService.addLogMessage("You don't have the spirituality required to ascend.","INJURY","EVENT");
+    if (this.characterService.characterState.attributes.spirituality.value < this.characterService.characterState.condenseSoulCoreCost) {
+      this.logService.addLogMessage("You don't have the spirituality required to ascend.", "INJURY", "EVENT");
       return;
     }
-    if (this.inventoryService.checkFor("spiritGem") < (this.soulCoreRank + 12) * 10 ){
-      this.logService.addLogMessage("You don't have the gem required to ascend.","INJURY","EVENT");
+    if (this.inventoryService.checkFor("spiritGem") < (this.soulCoreRank + 12) * 10) {
+      this.logService.addLogMessage("You don't have the gem required to ascend.", "INJURY", "EVENT");
       return;
     }
     this.characterService.condenseSoulCore();
     this.dialog.closeAll();
   }
 
-  reinforceMeridians(){
-    if (this.meridianRank >= 9){
-      this.logService.addLogMessage("You can't reinforce your meridians any further.","INJURY","EVENT");
+  reinforceMeridians() {
+    if (this.meridianRank >= 9) {
+      this.logService.addLogMessage("You can't reinforce your meridians any further.", "INJURY", "EVENT");
       return;
     }
-    if (this.characterService.characterState.attributes.spirituality.value < this.characterService.characterState.reinforceMeridiansCost){
-      this.logService.addLogMessage("You don't have the spirituality required to ascend.","INJURY","EVENT");
+    if (this.characterService.characterState.attributes.spirituality.value < this.characterService.characterState.reinforceMeridiansCost) {
+      this.logService.addLogMessage("You don't have the spirituality required to ascend.", "INJURY", "EVENT");
       return;
     }
-    if (this.inventoryService.checkFor("spiritGem") < (this.meridianRank + 16) * 10 ){
-      this.logService.addLogMessage("You don't have the gem required to ascend.","INJURY","EVENT");
+    if (this.inventoryService.checkFor("spiritGem") < (this.meridianRank + 16) * 10) {
+      this.logService.addLogMessage("You don't have the gem required to ascend.", "INJURY", "EVENT");
       return;
     }
 
@@ -196,17 +196,17 @@ export class StoreService {
     this.dialog.closeAll();
   }
 
-  upgradeBloodline(){
-    if (this.characterService.characterState.attributes.spirituality.value < this.characterService.characterState.bloodlineCost){
-      this.logService.addLogMessage("You don't have the spirituality required to ascend.","INJURY","EVENT");
+  upgradeBloodline() {
+    if (this.characterService.characterState.attributes.spirituality.value < this.characterService.characterState.bloodlineCost) {
+      this.logService.addLogMessage("You don't have the spirituality required to ascend.", "INJURY", "EVENT");
       return;
     }
-    if (this.characterService.characterState.bloodlineRank >= 9){
-      this.logService.addLogMessage("You can't enhance your bloodline any further.","INJURY","EVENT");
+    if (this.characterService.characterState.bloodlineRank >= 9) {
+      this.logService.addLogMessage("You can't enhance your bloodline any further.", "INJURY", "EVENT");
       return;
     }
-    if (this.homeService.home.type < this.bloodLineHomeRequirement.type){
-      this.logService.addLogMessage("You don't have a powerful enough home to ascend.","INJURY","EVENT");
+    if (this.homeService.home.type < this.bloodLineHomeRequirement.type) {
+      this.logService.addLogMessage("You don't have a powerful enough home to ascend.", "INJURY", "EVENT");
       return;
     }
     this.characterService.upgradeBloodline();
