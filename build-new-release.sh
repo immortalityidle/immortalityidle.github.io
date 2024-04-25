2150bbe89c06b5f86b28e59f094a2af87e6dd2fc
immortalityidle <103467158+immortalityidle@users.noreply.github.com>
2024-04-25 16:33:22 -0600

    Added a classic version just in case.

--- build-new-release.sh
+++ build-new-release.sh
@@ -6,12 +6,10 @@ rm -rf /tmp/docs
 cp -rf docs/ /tmp
 echo "Building"
 npm run build
-npm run build-experimental
-npm run build-experimental2
 echo "Creating new old version"
 rm -rf /tmp/docs/old
-rm -rf /tmp/docs/experimental
-rm -rf /tmp/docs/experimental2
 rm -rf docs/old
 cp -rf /tmp/docs docs/old
 sed -i 's/href=\"\/\"/href=\"\/old\/\"/' docs/old/index.html
+echo "restoring classic version"
+cp -rf /tmp/docs/classic docs/classic
