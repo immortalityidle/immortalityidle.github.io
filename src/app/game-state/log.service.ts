30cf399a5b2b5ec7ee9e6102cad801e2d2c31097
immortalityidle <cbradley0912@gmail.com>
2025-05-16 11:55:44 -0600

    Simplified main loop service.

--- src/app/game-state/log.service.ts
+++ src/app/game-state/log.service.ts
@@ -72,7 +72,7 @@ export class LogService {
   longTickCounter = 0;
 
   constructor(mainLoopService: MainLoopService) {
-    mainLoopService.frameSubject.subscribe(() => {
+    mainLoopService.tickSubject.subscribe(() => {
       this.updateLogTopics();
     });
 
